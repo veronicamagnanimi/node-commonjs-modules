73532e53557486ac6dc8b77d75e5dcc9058ba125
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-29 15:26:35 +0100

    feat: second exercise

--- app.js
+++ app.js
@@ -2,3 +2,8 @@
 const createFullName = require('./names');
 const result = createFullName('Veronica', 'Magnanimi');
 console.log(result)
+
+//importazione hobby
+const allMyHobbies = require('./hobbies');
+const iLike = allMyHobbies('Ginnastica Artistica', 'Recitazione', 'Viaggi');
+console.log(iLike);
